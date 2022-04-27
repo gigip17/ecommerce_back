27bb10bda16a8c6f29c2b58b07a78c65d831ae8a
gigip17 <70454351+gigip17@users.noreply.github.com>
2022-04-27 16:44:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-ca arrive
+const express = require('express');
+const app = express();
+const PORT = 4000
+
+app.get('/', (req,res) => {
+    res.send('hello Express !')
+})
+
+app.listen(PORT,()=> {
+    console.log(`Exemple app listening at localhost:${PORT}`)
+})
