6e211a72d9f75d0dec046d5e3a354ed3b80d0eff
gigip17 <70454351+gigip17@users.noreply.github.com>
2022-05-01 16:26:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
-const PORT = 4000
+const PORT = 4000;
+const MongoDBClient = require('./mongoClient');
 
+//API REst exemple
+const Product = require('./models/products');
+app.get('/products', async(req, res) => {
+    const products = await Product.find({})
+    try{
+        res.send(products)
+    }catch(e) {
+        res.status(500).send(err)
+    }
+})
+
+app.use(cors())
 app.get('/', (req,res) => {
     res.send('hello Express !')
 })
 
 app.listen(PORT,()=> {
     console.log(`Exemple app listening at localhost:${PORT}`)
+    MongoDBClient.initialize();
 })
